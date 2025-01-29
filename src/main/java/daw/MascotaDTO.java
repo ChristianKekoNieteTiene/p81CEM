2813cd79cc652fe910b9b6738a83eb7ee19f6c00
ChristianEysel <ceysmar2109@g.educaand.es>
2025-01-29 13:22:15 +0100

    conexion con base de datos

---
+++ src/main/java/daw/MascotaDTO.java
@@ -0,0 +1,22 @@
+
+package daw;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author christian
+ */
+public class MascotaDTO {
+    
+    private int id;
+    private String nombre;
+    private int nChip;
+    private double peso;
+    private LocalDate fechNac;
+    private String tipo;
+    private int idVet;
+}
+
+    
+
---
+++ src/main/java/daw/VeterinarioDTO.java
@@ -0,0 +1,18 @@
+
+package daw;
+
+
+/**
+ *
+ * @author christian
+ */
+public class VeterinarioDTO {
+    
+    private int idVet;
+    private String nifVet;
+    private String nomVet;
+    private String dirVet;
+    private String telVet;
+    private String emailVet;
+    
+}
